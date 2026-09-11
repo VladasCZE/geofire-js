09e26431d52a4c4c5dabd0d7f7c30a0165081e87
Vladimir Lizak <vladimir.lizak@gmail.com>
2026-09-10 23:43:37 -0500

    chore: migrate from lerna to npm workspaces and update rollup plugins

--- packages/geofire-common/rollup.config.js
+++ packages/geofire-common/rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolveModule from 'rollup-plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
