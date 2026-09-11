b68329311c48fce50038f67b64c7edb5feec3b84
Vladimir Lizak <vladimir.lizak@gmail.com>
2026-09-10 22:55:49 -0500

    chore: bump version to 7.0.0

--- packages/geofire/src/GeoFire.ts
+++ packages/geofire/src/GeoFire.ts
@@ -5,7 +5,7 @@
  * possibility of retrieving only those keys within a given geographic area -
  * all in realtime.
  *
- * GeoFire 0.0.0
+ * GeoFire 7.0.0
  * https://github.com/firebase/geofire-js/
  * License: MIT
  */
