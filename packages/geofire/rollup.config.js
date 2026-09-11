27e993f2976677861e8a08a3bb7a6557efa09202
Vladimir Lizak <vladimir.lizak@gmail.com>
2026-09-10 21:58:42 -0500

    Fix ESM import attribute error in rollup configs by using createRequire for package.json

--- packages/geofire/rollup.config.js
+++ packages/geofire/rollup.config.js
@@ -2,7 +2,9 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolveModule from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 const GLOBAL_NAME = 'geofire';
 
